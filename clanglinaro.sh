336cbc3871bcf907fa30ef2c023ab61302e24d53
fredslbn <138670465+fredslbn@users.noreply.github.com>
2024-03-24 16:37:39 +0700

    Update clanglinaro.sh

--- clanglinaro.sh
+++ clanglinaro.sh
@@ -8,6 +8,8 @@
 # Specify Kernel Directory
 KERNEL_DIR="$(pwd)"
 
+git submodule update --init --recursive --remote
+
 ##----------------------------------------------------------##
 # Device Name and Model
 MODEL=Xiaomi
